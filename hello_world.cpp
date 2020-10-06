4af5e0cf7d2dfcf8cf2fac310bbc8c090b16e4f1
ARcheMatch <aleksandr.kostrikin@mail.ru>
2020-10-06 08:23:14 +0300

    Code quality improved

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <string>
 
+/**
+ * main
+ * @brief reads username from standart input stream, writes greetings message 
+ * to standart output stream
+ * @returns 0 if no error occures
+ */
 int main(){
-    std::string name;
+    std::string name;// Read name from standart input stream
     std::cin>>name;
-    std::cout<<"Hello world from "<<name<<std::endl;
+    std::cout<<"Hello world from "<<name<<std::endl;// Write greeting string
     return 0;
 }
