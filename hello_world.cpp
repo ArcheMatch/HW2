37020e0fc69276db354454d18d4560995a6bb7f3
ARcheMatch <aleksandr.kostrikin@mail.ru>
2020-10-06 08:45:33 +0300

    add clang-format and format files using it

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,13 +3,16 @@
 
 /**
  * main
- * @brief reads username from standart input stream, writes greetings message 
+ * @brief reads username from standart input stream, writes greetings message
  * to standart output stream
  * @returns 0 if no error occures
  */
-int main(){
-    std::string name;// Read name from standart input stream
-    std::cin>>name;
-    std::cout<<"Hello world from "<<name<<std::endl;// Write greeting string
-    return 0;
+int
+main()
+{
+  std::string name; // Read name from standart input stream
+  std::cin >> name;
+  std::cout << "Hello world from " << name
+            << std::endl; // Write greeting string
+  return 0;
 }
